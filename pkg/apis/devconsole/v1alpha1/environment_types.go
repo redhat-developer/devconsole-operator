6ac7c65b9f051260fe66fac8f7b5c53adfad979b
Alexey Kazakov <alkazako@redhat.com>
2019-01-29 18:23:02 -0800

    Env CRD

--- pkg/apis/devconsole/v1alpha1/environment_types.go
+++ pkg/apis/devconsole/v1alpha1/environment_types.go
@@ -1,16 +1,31 @@
 package v1alpha1
 
 import (
+	clusterv1alpha1 "github.com/alexeykazakov/devconsole/pkg/apis/clusterregistry/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // EnvironmentSpec defines the desired state of Environment
 type EnvironmentSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of Environment
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+
+	// Type contains the type of the environment.
+	// should be treated as "deploy" if nil
+	// +optional
+	Type string `json:"name,omitempty" protobuf:"bytes,2,opt,name=type"`
+
+	// Cluster contains information about a cluster.
+	// If empty then the current cluster should be used.
+	// +optional
+	Cluster clusterv1alpha1.Cluster `json:"cluster,omitempty" protobuf:"bytes,2,opt,name=cluster"`
+
+	// Namespace contains the namespace of the environment.
+	// If empty then the current namespace should be used.
+	// +optional
+	Namespace string `json:"namespace,omitempty" protobuf:"bytes,3,opt,name=namespace"`
 }
 
 // EnvironmentStatus defines the observed state of Environment
