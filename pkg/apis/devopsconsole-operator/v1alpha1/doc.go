cf023c28b7b7cb31d4302a7e5c7f02da151ae9c0
Shoubhik Bose <shbose@redhat.com>
2019-02-14 23:39:01 -0500

    fix group name

--- pkg/apis/devopsconsole-operator/v1alpha1/doc.go
+++ pkg/apis/devopsconsole-operator/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the devopsconsole-operator v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=devopsconsole-operator.openshift.io
+// +groupName=devopsconsole.openshift.io
 package v1alpha1
