8337366c9bc52dad1c4337c4778dd0b71ff8a7f3
Corinne Krych <corinnekrych@gmail.com>
2019-03-05 20:29:44 +0100

    create imagestreams and buildconfig for nodejs runtime

--- pkg/apis/devopsconsole-operator/v1alpha1/zz_generated.openapi.go
+++ pkg/apis/devopsconsole-operator/v1alpha1/zz_generated.openapi.go
@@ -13,16 +13,17 @@ import (
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 	return map[string]common.OpenAPIDefinition{
-		"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvDeployment": schema_pkg_apis_devopsconsole_operator_v1alpha1_EnvDeployment(ref),
-		"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.Environment":   schema_pkg_apis_devopsconsole_operator_v1alpha1_Environment(ref),
+		"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.GitSource":       schema_pkg_apis_devopsconsole_operator_v1alpha1_GitSource(ref),
+		"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.GitSourceSpec":   schema_pkg_apis_devopsconsole_operator_v1alpha1_GitSourceSpec(ref),
+		"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.GitSourceStatus": schema_pkg_apis_devopsconsole_operator_v1alpha1_GitSourceStatus(ref),
 	}
 }
 
-func schema_pkg_apis_devopsconsole_operator_v1alpha1_EnvDeployment(ref common.ReferenceCallback) common.OpenAPIDefinition {
+func schema_pkg_apis_devopsconsole_operator_v1alpha1_GitSource(ref common.ReferenceCallback) common.OpenAPIDefinition {
 	return common.OpenAPIDefinition{
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
-				Description: "EnvDeployment is the Schema for the envdeployments API",
+				Description: "GitSource is the Schema for the gitsources API",
 				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
 					"kind": {
@@ -46,62 +47,100 @@ func schema_pkg_apis_devopsconsole_operator_v1alpha1_EnvDeployment(ref common.Re
 					},
 					"spec": {
 						SchemaProps: spec.SchemaProps{
-							Ref: ref("github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvDeploymentSpec"),
+							Ref: ref("github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.GitSourceSpec"),
 						},
 					},
 					"status": {
 						SchemaProps: spec.SchemaProps{
-							Ref: ref("github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvDeploymentStatus"),
+							Ref: ref("github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.GitSourceStatus"),
 						},
 					},
 				},
 			},
 		},
 		Dependencies: []string{
-			"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvDeploymentSpec", "github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvDeploymentStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
+			"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.GitSourceSpec", "github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.GitSourceStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
 	}
 }
 
-func schema_pkg_apis_devopsconsole_operator_v1alpha1_Environment(ref common.ReferenceCallback) common.OpenAPIDefinition {
+func schema_pkg_apis_devopsconsole_operator_v1alpha1_GitSourceSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
 	return common.OpenAPIDefinition{
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
-				Description: "Environment is the Schema for the environments API",
+				Description: "GitSourceSpec defines the desired state of GitSource",
 				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
-					"kind": {
+					"url": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds",
+							Description: "URL of the git repo",
 							Type:        []string{"string"},
 							Format:      "",
 						},
 					},
-					"apiVersion": {
+					"ref": {
 						SchemaProps: spec.SchemaProps{
-							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#resources",
+							Description: "Ref is a git reference. Optional. \"master\" is used by default.",
 							Type:        []string{"string"},
 							Format:      "",
 						},
 					},
-					"metadata": {
+					"contextDir": {
 						SchemaProps: spec.SchemaProps{
-							Ref: ref("k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"),
+							Description: "ContextDir is a path to subfolder in the repo. Optional.",
+							Type:        []string{"string"},
+							Format:      "",
 						},
 					},
-					"spec": {
+					"httpProxy": {
 						SchemaProps: spec.SchemaProps{
-							Ref: ref("github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvironmentSpec"),
+							Description: "HttpProxy is optional.",
+							Type:        []string{"string"},
+							Format:      "",
 						},
 					},
-					"status": {
+					"httpsProxy": {
 						SchemaProps: spec.SchemaProps{
-							Ref: ref("github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvironmentStatus"),
+							Description: "HttpsProxy is optional.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"noProxy": {
+						SchemaProps: spec.SchemaProps{
+							Description: "NoProxy can be used to specify domains for which no proxying should be performed. Optional.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"secretRef": {
+						SchemaProps: spec.SchemaProps{
+							Description: "SecretRef refers to the secret that contains credentials to access the git repo. Optional.",
+							Ref:         ref("github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.SecretRef"),
+						},
+					},
+					"flavor": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Flavor of the git provider like github, gitlab, bitbucket, generic, etc. Optional.",
+							Type:        []string{"string"},
+							Format:      "",
 						},
 					},
 				},
+				Required: []string{"url"},
 			},
 		},
 		Dependencies: []string{
-			"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvironmentSpec", "github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.EnvironmentStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
+			"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole-operator/v1alpha1.SecretRef"},
+	}
+}
+
+func schema_pkg_apis_devopsconsole_operator_v1alpha1_GitSourceStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "GitSourceStatus defines the observed state of GitSource",
+				Type:        []string{"object"},
+			},
+		},
 	}
 }
---
+++ pkg/controller/component/component_controller.go
@@ -0,0 +1,252 @@
+package component
+
+import (
+	"context"
+	"fmt"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"strconv"
+
+	componentsv1alpha1 "github.com/redhat-developer/devopsconsole-operator/pkg/apis/devopsconsole/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	buildv1 "github.com/openshift/api/build/v1"
+	imagev1 "github.com/openshift/api/image/v1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+var log = logf.Log.WithName("controller_component")
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new Component Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileComponent{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("component-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource Component
+	err = c.Watch(&source.Kind{Type: &componentsv1alpha1.Component{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to secondary resource Pods and requeue the owner Component
+	//err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+	//	IsController: true,
+	//	OwnerType:    &componentsv1alpha1.Component{},
+	//})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileComponent{}
+var buildTyppeImages = map[string]string{"nodejs":"nodeshift/centos7-s2i-nodejs:10.x"}
+
+// ReconcileComponent reconciles a Component object
+type ReconcileComponent struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a Component object and makes changes based on the state read
+// and what is in the Component.Spec
+// Note:
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcileComponent) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling Component")
+
+	// Fetch the Component instance
+	instance := &componentsv1alpha1.Component{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	log.Info("============================================================")
+	log.Info(fmt.Sprintf("***** Reconciling Component %s, namespace %s", request.Name, request.Namespace))
+	log.Info(fmt.Sprintf("** Creation time : %s", instance.ObjectMeta.CreationTimestamp))
+	log.Info(fmt.Sprintf("** Resource version : %s", instance.ObjectMeta.ResourceVersion))
+	log.Info(fmt.Sprintf("** Generation version : %s", strconv.FormatInt(instance.ObjectMeta.Generation, 10)))
+	log.Info(fmt.Sprintf("** Deletion time : %s", instance.ObjectMeta.DeletionTimestamp))
+	log.Info("============================================================")
+
+	// Assign the generated ResourceVersion to the resource status
+	if instance.Status.RevNumber == "" {
+		instance.Status.RevNumber = instance.ObjectMeta.ResourceVersion
+	}
+
+	if !instance.ObjectMeta.DeletionTimestamp.IsZero() {
+		log.Info( "** DELETION **")
+	}
+
+	// We only call the pipeline when the component has been created
+	// and if the Status Revision Number is the same
+	if instance.Status.RevNumber == instance.ObjectMeta.ResourceVersion {
+		// Create an empty image name "nodejs-output"
+		newImageForOutput := newImageStream(instance.Namespace, instance.Name)
+		err = r.client.Create(context.TODO(), newImageForOutput)
+		if err != nil {
+			log.Error(err, "** Creating new OUTPUT image fails **")
+			return reconcile.Result{}, err
+		}
+		log.Info("** Image stream for OUTPUT created **")
+		if err := controllerutil.SetControllerReference(instance, newImageForOutput, r.scheme); err != nil {
+			log.Error(err, "** Setting owner reference fails **")
+			return reconcile.Result{}, err
+		}
+
+		// Create an empty image name "nodejs-runtime"
+		newImageForRuntime := newImageStreamFromDocker(instance.Namespace, instance.Name, instance.Spec.BuildType)
+		err = r.client.Create(context.TODO(), newImageForRuntime)
+		if err != nil {
+			log.Error(err, "** Creating new RUNTIME image fails **")
+			return reconcile.Result{}, err
+		}
+		log.Info("** Image stream for RUNTIME created **")
+		if err := controllerutil.SetControllerReference(instance, newImageForRuntime, r.scheme); err != nil {
+			log.Error(err, "** Setting owner reference fails **")
+			return reconcile.Result{}, err
+		}
+
+		log.Info("** Build config created **")
+
+		// Create build config with s2i
+		bc := generateBuildConfig(instance.Namespace, instance.Name, instance.Spec.Codebase, "master")
+		err = r.client.Create(context.TODO(), &bc)
+		if err != nil {
+			log.Error(err, "** BuildConfig creation fails **")
+			return reconcile.Result{}, err
+		}
+		log.Info("** BuildConfig created **")
+	}
+
+	return reconcile.Result{}, nil
+}
+
+
+func newImageStreamFromDocker(namespace string, name string, buildType string) *imagev1.ImageStream {
+	labels := map[string]string{
+		"app": name,
+	}
+	if _, ok := buildTyppeImages[buildType]; !ok {
+		return nil
+	}
+	return &imagev1.ImageStream{ObjectMeta:metav1.ObjectMeta{
+		Name: name+"-runtime",
+		Namespace: namespace,
+		Labels:    labels,
+	}, Spec: imagev1.ImageStreamSpec{
+		LookupPolicy: imagev1.ImageLookupPolicy{ //optional?
+			Local:false,
+		},
+		Tags:[]imagev1.TagReference{
+			{
+				Name:"latest",
+				From:&corev1.ObjectReference{
+					Kind: "DockerImage",
+					Name: buildTyppeImages[buildType],
+				},
+			},
+		},
+	}}
+}
+func newImageStream(namespace string, name string) *imagev1.ImageStream {
+	labels := map[string]string{
+		"app": name,
+	}
+	return &imagev1.ImageStream{ObjectMeta:metav1.ObjectMeta{
+		Name: name+"-output",
+		Namespace: namespace,
+		Labels:    labels,
+	}}
+}
+
+func getMetaObj(name string, imageNamespace string) metav1.ObjectMeta {
+	labels := map[string]string{
+		"app": name,
+	}
+	return metav1.ObjectMeta{Name: name, Namespace:imageNamespace, Labels: labels}
+}
+
+func generateBuildConfig(namespace string, name string, gitURL string, gitRef string) buildv1.BuildConfig {
+	buildSource := buildv1.BuildSource{
+		Git: &buildv1.GitBuildSource{
+			URI: gitURL,
+			Ref: gitRef,
+		},
+		Type: buildv1.BuildSourceGit,
+	}
+	incremental := true
+	return buildv1.BuildConfig{
+		ObjectMeta: getMetaObj(name + "-bc", namespace),
+		Spec: buildv1.BuildConfigSpec{
+			CommonSpec: buildv1.CommonSpec{
+				Output: buildv1.BuildOutput{
+					To: &corev1.ObjectReference{
+						Kind: "ImageStreamTag",
+						Name: name + "-output:latest",
+					},
+				},
+				Source: buildSource,
+				Strategy: buildv1.BuildStrategy{
+					SourceStrategy: &buildv1.SourceBuildStrategy{
+						From: corev1.ObjectReference{
+							Kind:      "ImageStreamTag",
+							Name:      name + "-runtime:latest",
+							Namespace: namespace,
+						},
+						Incremental: &incremental,
+					},
+				},
+			},
+			Triggers:[]buildv1.BuildTriggerPolicy{ //todo add gh webhook secret
+				{
+					Type: "ConfigChange",
+				}, {
+					Type: "ImageChange",
+					ImageChange: &buildv1.ImageChangeTrigger{},
+				},
+			},
+		},
+	}
+}
