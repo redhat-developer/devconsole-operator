0e1e9d7b4e0725d8accb60aae86cd79db7bbcfd7
tinakurian <tkurian@redhat.com>
2019-03-06 15:17:57 -0500

    adding ci job to build and deploy operator

---
+++ cico_build_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. cico_setup.sh
+
+load_jenkins_vars
+ if [[ ! -f .cico-prepare ]]; then
+    install_deps
+    prepare
+    run_tests_without_coverage;
+    touch .cico-prepare
+fi
+
+deploy;
