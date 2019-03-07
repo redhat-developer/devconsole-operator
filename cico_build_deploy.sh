45aa703a6844ac598ebea6bba78f1206cc2aabbc
Shoubhik Bose <shbose@redhat.com>
2019-03-06 19:45:25 -0500

    simplify cd code

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -2,12 +2,8 @@
 
 . cico_setup.sh
 
-load_jenkins_vars
- if [[ ! -f .cico-prepare ]]; then
-    install_deps
-    prepare
-    run_tests_without_coverage;
-    touch .cico-prepare
-fi
+cico_setup;
+
+run_tests_without_coverage;
 
 deploy;
