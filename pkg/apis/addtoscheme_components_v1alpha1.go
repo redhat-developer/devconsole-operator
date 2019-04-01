64dee9a108de31f6eab59d14980f5b6d5fccafd5
tinakurian <tkurian@redhat.com>
2019-04-01 10:27:44 -0400

    fixing imports

--- pkg/apis/addtoscheme_components_v1alpha1.go
+++ pkg/apis/addtoscheme_components_v1alpha1.go
@@ -1,7 +1,7 @@
 package apis
 
 import (
-	"github.com/redhat-developer/devopsconsole-operator/pkg/apis/devconsole/v1alpha1"
+	"github.com/redhat-developer/devconsole-operator/pkg/apis/devconsole/v1alpha1"
 )
 
 func init() {
