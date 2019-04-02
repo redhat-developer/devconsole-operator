9cbf1a04f3a40e7a255b8bcc49cec99a44c8b330
Avni Sharma <16avnisharma@gmail.com>
2019-04-02 15:18:22 +0530

    Clean up of catalog source and subscription
    Fixes #65

--- make/test.mk
+++ make/test.mk
@@ -80,6 +80,8 @@ olm-integration-setup: olm-integration-cleanup
 .PHONY: olm-integration-cleanup
 olm-integration-cleanup: get-test-namespace
 	$(Q)oc login -u system:admin
+	$(Q)-oc delete catalogsources -n olm my-catalog
+	$(Q)-oc delete subscriptions -n operators my-devconsole
 	$(Q)-oc delete project $(TEST_NAMESPACE)  --wait
 
 #-------------------------------------------------------------------------------
