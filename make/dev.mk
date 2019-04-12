2bac8665f3ef0461aef80f150cea3104a7e93134
Baiju Muthukadan <baiju.m.mail@gmail.com>
2019-04-12 20:50:27 +0530

    Use `KUBECONFIG` instead of username & password (#110)
    The OpenShift 4 installer creates a Kubernetes configuration file for authentication.  The `oc` command and Operator SDK clients look for an environment variable named `KUBECONFIG` pointing to this configuration file. https://kubernetes.io/docs/concepts/configuration/organize-cluster-access-kubeconfig/
    
    The `oc` command also works with `KUBECONFIG` configuration. And it is used for cleanup. Whereas the Operator SDK framework expects `KUBECONFIG`. This makes `oc login` is redundant.
    https://github.com/operator-framework/operator-sdk/blob/master/doc/user-guide.md#2-run-locally-outside-the-cluster
    
    * This PR also set OpenShift 4 as the default version as the test target

--- make/dev.mk
+++ make/dev.mk
@@ -11,9 +11,7 @@ REGISTRY_URI=quay.io
 DEVCONSOLE_OPERATOR_IMAGE?=quay.io/redhat-developers/devconsole-operator
 TIMESTAMP:=$(shell date +%s)
 TAG?=$(GIT_COMMIT_ID_SHORT)-$(TIMESTAMP)
-OPENSHIFT_VERSION?=3
-OC_LOGIN_USERNAME?=system:admin
-OC_LOGIN_PASSWORD?=admin
+OPENSHIFT_VERSION?=4
 
 .PHONY: create-resources
 create-resources:
