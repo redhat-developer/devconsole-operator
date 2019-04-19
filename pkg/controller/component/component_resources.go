57662862e685c2f5b55d8d8a0e639a2e196e7d27
Corinne Krych <corinnekrych@gmail.com>
2019-04-19 14:43:54 +0200

    replace general instance by component cp

--- pkg/controller/component/component_resources.go
+++ pkg/controller/component/component_resources.go
@@ -18,15 +18,15 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func newImageStreamFromDocker(cr *devconsoleapi.Component) *imagev1.ImageStream {
-	labels := resource.GetLabelsForCR(cr)
+func newImageStreamFromDocker(cp *devconsoleapi.Component) *imagev1.ImageStream {
+	labels := resource.GetLabelsForCR(cp)
 
-	if _, ok := buildTypeImages[cr.Spec.BuildType]; !ok {
+	if _, ok := buildTypeImages[cp.Spec.BuildType]; !ok {
 		return nil
 	}
 	return &imagev1.ImageStream{ObjectMeta: metav1.ObjectMeta{
-		Name:      cr.Spec.BuildType,
-		Namespace: cr.Namespace,
+		Name:      cp.Spec.BuildType,
+		Namespace: cp.Namespace,
 		Labels:    labels,
 	}, Spec: imagev1.ImageStreamSpec{
 		LookupPolicy: imagev1.ImageLookupPolicy{
@@ -37,24 +37,24 @@ func newImageStreamFromDocker(cr *devconsoleapi.Component) *imagev1.ImageStream 
 				Name: "latest",
 				From: &corev1.ObjectReference{
 					Kind: "DockerImage",
-					Name: buildTypeImages[cr.Spec.BuildType],
+					Name: buildTypeImages[cp.Spec.BuildType],
 				},
 			},
 		},
 	}}
 }
 
-func newOutputImageStream(cr *devconsoleapi.Component) *imagev1.ImageStream {
-	labels := resource.GetLabelsForCR(cr)
+func newOutputImageStream(cp *devconsoleapi.Component) *imagev1.ImageStream {
+	labels := resource.GetLabelsForCR(cp)
 	return &imagev1.ImageStream{ObjectMeta: metav1.ObjectMeta{
-		Name:      cr.Name,
-		Namespace: cr.Namespace,
+		Name:      cp.Name,
+		Namespace: cp.Namespace,
 		Labels:    labels,
 	}}
 }
 
-func newBuildConfig(cr *devconsoleapi.Component, builder *imagev1.ImageStream, gitSource *devconsoleapi.GitSource, secret *corev1.Secret) *buildv1.BuildConfig {
-	labels := resource.GetLabelsForCR(cr)
+func newBuildConfig(cp *devconsoleapi.Component, builder *imagev1.ImageStream, gitSource *devconsoleapi.GitSource, secret *corev1.Secret) *buildv1.BuildConfig {
+	labels := resource.GetLabelsForCR(cp)
 	buildSource := buildv1.BuildSource{
 		Git: &buildv1.GitBuildSource{
 			URI: gitSource.Spec.URL,
@@ -69,13 +69,13 @@ func newBuildConfig(cr *devconsoleapi.Component, builder *imagev1.ImageStream, g
 	}
 	incremental := true
 	return &buildv1.BuildConfig{
-		ObjectMeta: metav1.ObjectMeta{Name: cr.Name, Namespace: cr.Namespace, Labels: labels},
+		ObjectMeta: metav1.ObjectMeta{Name: cp.Name, Namespace: cp.Namespace, Labels: labels},
 		Spec: buildv1.BuildConfigSpec{
 			CommonSpec: buildv1.CommonSpec{
 				Output: buildv1.BuildOutput{
 					To: &corev1.ObjectReference{
 						Kind: "ImageStreamTag",
-						Name: cr.Name + ":latest",
+						Name: cp.Name + ":latest",
 					},
 				},
 				Source: buildSource,
@@ -102,8 +102,8 @@ func newBuildConfig(cr *devconsoleapi.Component, builder *imagev1.ImageStream, g
 	}
 }
 
-func newDeploymentConfig(cr *devconsoleapi.Component, output *imagev1.ImageStream, containerPorts []corev1.ContainerPort) *v1.DeploymentConfig {
-	labels := resource.GetLabelsForCR(cr)
+func newDeploymentConfig(cp *devconsoleapi.Component, output *imagev1.ImageStream, containerPorts []corev1.ContainerPort) *v1.DeploymentConfig {
+	labels := resource.GetLabelsForCR(cp)
 	if containerPorts == nil {
 		containerPorts = []corev1.ContainerPort{{
 			ContainerPort: 8080,
@@ -112,8 +112,8 @@ func newDeploymentConfig(cr *devconsoleapi.Component, output *imagev1.ImageStrea
 	}
 	return &v1.DeploymentConfig{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name,
-			Namespace: cr.Namespace,
+			Name:      cp.Name,
+			Namespace: cp.Namespace,
 			Labels:    labels,
 		},
 		Spec: v1.DeploymentConfigSpec{
@@ -124,8 +124,8 @@ func newDeploymentConfig(cr *devconsoleapi.Component, output *imagev1.ImageStrea
 			Selector: labels,
 			Template: &corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      cr.Name,
-					Namespace: cr.Namespace,
+					Name:      cp.Name,
+					Namespace: cp.Namespace,
 					Labels:    labels,
 				},
 				Spec: corev1.PodSpec{
@@ -157,14 +157,14 @@ func newDeploymentConfig(cr *devconsoleapi.Component, output *imagev1.ImageStrea
 	}
 }
 
-func newService(cr *devconsoleapi.Component, port int32) (*corev1.Service, error) {
-	labels := resource.GetLabelsForCR(cr)
+func newService(cp *devconsoleapi.Component, port int32) (*corev1.Service, error) {
+	labels := resource.GetLabelsForCR(cp)
 	if port > 65536 || port < 1024 {
 		return nil, fmt.Errorf("port %d is out of range [1024-65535]", port)
 	}
 	var svcPorts []corev1.ServicePort
 	svcPort := corev1.ServicePort{
-		Name:       cr.Name + "-tcp",
+		Name:       cp.Name + "-tcp",
 		Port:       port,
 		Protocol:   corev1.ProtocolTCP,
 		TargetPort: intstr.FromInt(int(port)),
@@ -172,36 +172,36 @@ func newService(cr *devconsoleapi.Component, port int32) (*corev1.Service, error
 	svcPorts = append(svcPorts, svcPort)
 	svc := &corev1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name,
-			Namespace: cr.Namespace,
+			Name:      cp.Name,
+			Namespace: cp.Namespace,
 			Labels:    labels,
 		},
 		Spec: corev1.ServiceSpec{
 			Ports: svcPorts,
 			Selector: map[string]string{
-				"deploymentconfig": cr.Name,
+				"deploymentconfig": cp.Name,
 			},
 		},
 	}
 	return svc, nil
 }
 
-func newRoute(cr *devconsoleapi.Component) *routev1.Route {
-	labels := resource.GetLabelsForCR(cr)
+func newRoute(cp *devconsoleapi.Component) *routev1.Route {
+	labels := resource.GetLabelsForCR(cp)
 
 	route := &routev1.Route{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name,
-			Namespace: cr.Namespace,
+			Name:      cp.Name,
+			Namespace: cp.Namespace,
 			Labels:    labels,
 		},
 		Spec: routev1.RouteSpec{
 			To: routev1.RouteTargetReference{
 				Kind: "Service",
-				Name: cr.Name,
+				Name: cp.Name,
 			},
 			Port: &routev1.RoutePort{
-				TargetPort: intstr.IntOrString{IntVal: cr.Spec.Port, StrVal: fmt.Sprintf("%d-tcp", cr.Spec.Port)},
+				TargetPort: intstr.IntOrString{IntVal: cp.Spec.Port, StrVal: fmt.Sprintf("%d-tcp", cp.Spec.Port)},
 			},
 		},
 	}
