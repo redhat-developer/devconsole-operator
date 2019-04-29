8adac20279b550eb2f095147bed4d4ff97f49f5c
Avni Sharma <16avnisharma@gmail.com>
2019-04-30 01:39:52 +0530

    Changes in yaml file and consoledeveloper.sh
    Delete the already existing pod in the openshift-console namespace
    Because it's a Deployment, Kubernetes will automatically recreate the pod and pull the latest image always.
    Have also updated the image pull policy to Always in the yamls/redeploy-console-operator.yaml

--- install_devconsole/consoledeveloper.sh
+++ install_devconsole/consoledeveloper.sh
@@ -10,6 +10,16 @@ while [ "$(oc get pods --field-selector=status.phase=Running -n openshift-consol
 do
     sleep 1s
 done
+
 oc scale --replicas 1 deployment console --namespace openshift-console
+#Delete the already existing pod in the openshift-console namespace
+#Because it's a Deployment, Kubernetes will automatically recreate the pod and pull the latest image.
+#Have also updated the image pull policy to Always in the yamls/redeploy-console-operator.yaml
+
+CONSOLE_POD="$(oc get pods -o=name -n openshift-console | cut -d'/' -f2- | cut -f 1 -d "-" | head -n 1)"
+CONSOLE_POD_NAME="$(oc get pods -o=name -n openshift-console | cut -d'/' -f2- | cut -d'-' -f1- | head -n 1)"
+if echo "${CONSOLE_POD}" == "console";then
+    oc delete pod ${CONSOLE_POD_NAME} -n openshift-console
+fi
 sh ./devconsole.sh
 sh ./create_user.sh
