b913a7463ce7c4bac833613d3df0117755afd46f
Baiju Muthukadan <baiju.m.mail@gmail.com>
2019-04-30 21:50:45 +0530

    Wait for CRDs before actual test

--- make/test.mk
+++ make/test.mk
@@ -96,6 +96,9 @@ test-e2e-olm-ci: ./vendor
 	$(Q)sed -e "s,REPLACE_IMAGE,registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:devconsole-operator-registry," ./test/e2e/catalog_source_OS4.yaml | oc apply -f -
 	$(Q)oc apply -f ./test/e2e/subscription_OS4.yaml
 	$(eval DEPLOYED_NAMESPACE := openshift-operators)
+	$(Q)oc wait crd/components.devconsole.openshift.io --for=condition=Established --timeout=300s
+	$(Q)oc wait crd/gitsourceanalyses.devconsole.openshift.io --for=condition=Established --timeout=300s
+	$(Q)oc wait crd/gitsources.devconsole.openshift.io --for=condition=Established --timeout=300s
 	$(Q)operator-sdk test local ./test/e2e --no-setup --go-test-flags "-v -timeout=15m"
 
 .PHONY: test-e2e-ci
