21a984acea5a356b22f9794727c67638a078b842
Baiju Muthukadan <baiju.m.mail@gmail.com>
2019-05-08 22:39:27 +0530

    Correct the steps for upgrade (#187)

--- make/test.mk
+++ make/test.mk
@@ -185,8 +185,11 @@ endif
 
 .PHONY: test-upgrade-ci
 test-upgrade-ci:
-	$(Q)sed -e "s,REPLACE_IMAGE,registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:devconsole-operator-registry-next," ./test/e2e/catalog_source_OS4.yaml | oc apply -f -
+	$(Q)sed -e "s,REPLACE_IMAGE,registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:devconsole-operator-registry," ./test/e2e/catalog_source_OS4.yaml | oc apply -f -
 	$(Q)oc apply -f ./test/e2e/subscription_OS4.yaml
 	$(eval DEPLOYED_NAMESPACE := openshift-operators)
+	$(Q)./hack/check-crds.sh
+	$(Q)operator-sdk test local ./test/e2e --no-setup --go-test-flags "-v -timeout=15m"
+	$(Q)sed -e "s,REPLACE_IMAGE,registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:devconsole-operator-registry-next," ./test/e2e/catalog_source_OS4.yaml | oc apply -f -
 	$(Q)./hack/check-crds-upgrade.sh
 	$(Q)operator-sdk test local ./test/e2e --no-setup --go-test-flags "-v -timeout=15m"
